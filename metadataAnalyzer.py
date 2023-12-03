55dd5063cd30b9cfdb424e117d8d38b13c6c7223
unknown <dml3483@rit.edu>
2023-12-03 14:25:07 -0500

    Added some stuff

--- metadataAnalyzer.py
+++ metadataAnalyzer.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 import json
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 def run_program_for_file(file_path):
     # Replace 'exiftool' with the actual path to your exiftool executable
@@ -12,7 +14,6 @@ def run_program_for_file(file_path):
         
         # Parse the JSON result
         json_result = json.loads(result.stdout)
-        #print(json_result)
         # Extract the file name without extension
         file_name = os.path.splitext(os.path.basename(file_path))[0]
         
@@ -20,9 +21,9 @@ def run_program_for_file(file_path):
         file_data[file_name] = json_result
         
         # Optionally, write the JSON object to a separate file
-        output_file_path = os.path.join(output_directory, f'{file_name}.json')
-        with open(output_file_path, 'w') as output_file:
-            json.dump(json_result, output_file, indent=2)
+        # output_file_path = os.path.join(output_directory, f'{file_name}.json')
+        # with open(output_file_path, 'w') as output_file:
+        #     json.dump(json_result, output_file, indent=2)
         
     except subprocess.CalledProcessError as e:
         print(f"Error running exiftool for {file_path}: {e}")
@@ -57,6 +58,35 @@ def main():
         print(f"Directory not found: {directory_path}")
     except Exception as e:
         print(f"An error occurred: {e}")
+
+    file_names = []
+    page_counts = []
+
+    for data_str in file_data:
+        try:
+            data = json.loads(data_str)
+            file_names.append(data.get('SourceFile', 'Unknown'))
+            
+            # Ensure 'PDF' is present in the loaded data
+            if 'PDF' in data:
+                tempList = data["PDF"]
+                print(tempList[2])
+                page_counts.append(tempList[2])
+            else:
+                page_counts.append(0)
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON for data: {data_str}")
+        except Exception as e:
+            print(f"Error processing data: {e}")
+
+
+    plt.figure(figsize=(20, 12))
+
+    sns.barplot(x=file_names, y=page_counts)
+    plt.xlabel('File Names')
+    plt.ylabel('Number of Pages')
+    plt.title('Number of Pages for Each PDF File')
+    plt.show()
 
 if __name__ == "__main__":
     main()
